38fe52287332ad7b6764c299d8844e3b6ef18d2d
Kishan Sudani <86771928+D3athGr1p@users.noreply.github.com>
2024-05-22 17:48:12 +0530

    simple server with webpage response

--- RUST/web_server/src/main.rs
+++ RUST/web_server/src/main.rs
@@ -1,21 +1,33 @@
 use std::{
     fs,
-    io::{Read, Write},
+    io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
 };
 
 fn handle_incoming_request(mut stream: TcpStream) {
-    let mut buf = [0; 1024];
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    stream.read(&mut buf).unwrap();
+    let status_line = "HTTP/1.1";
+    let mut response_content: String = String::new();
+    let mut response_content_length = 0;
+    let mut response = String::new();
 
-    let response_content = fs::read_to_string("index.html").unwrap();
-
-    let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-        response_content.len(),
-        response_content
-    );
+    if request_line == "GET / HTTP/1.1" {
+        response_content = fs::read_to_string("index.html").unwrap();
+        response_content_length = response_content.len();
+        response = format!(
+            "{} 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            status_line, response_content_length, response_content
+        );
+    } else {
+        response_content = fs::read_to_string("404.html").unwrap();
+        response_content_length = response_content.len();
+        response = format!(
+            "{} 404 Not Found\r\nContent-Length: {}\r\n\r\n{}",
+            status_line, response_content_length, response_content
+        );
+    }
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
