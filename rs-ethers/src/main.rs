f184348d0cddb111c8a0c023cf56f7537fbae123
Kishan Sudani <86771928+D3athGr1p@users.noreply.github.com>
2024-05-28 18:20:43 +0530

    initial commit

---
+++ rs-ethers/src/main.rs
@@ -0,0 +1,24 @@
+use ethers::{types::H160, utils::parse_ether};
+
+mod client;
+
+use client::EtherClient;
+use dotenv::dotenv;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    dotenv().ok();
+
+    let _: String = std::env::var("SIGNER_PRIVET_KEY").expect("Failed to read signer privet key");
+
+    let test = EtherClient::new().unwrap();
+
+    let to_adr: H160 = "0x000000000000000000000000000000000000dead"
+        .parse()
+        .unwrap();
+
+    let val = parse_ether(1u64).unwrap();
+
+    // let _ = test.get_block(10).await;
+    let _ = test.create_tx(to_adr, val).unwrap();
+}
